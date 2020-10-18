025000b2350452feb9fd12565a5c509ad25487b3
Ramy <ramy.ahmed.143@gmail.com>
2020-10-18 21:26:21 +0200

    make it better

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import { NestFactory, Reflector } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 import { DefaultValidationPipe } from './common/pipe/default-validation.pipe';
 import { Swagger } from './common/utils/swagger';
 import { JwtAuthGuard } from './modules/auth/guards/jwt-auth.guard';
 require('dotenv').config()
-import * as path from 'path';
-import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, { cors: true });
@@ -27,4 +26,6 @@ async function bootstrap() {
 
   await app.listen(process.env.PORT, () => console.log("Listening on port ", process.env.PORT));
 }
+
+//due to lake of Time I was Not Able To Write Tests
 bootstrap();
